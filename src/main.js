983021ba3f3c4d1fa066f9ae006012c38bbf1f3c
wnsor <nilay@evangerion.org>
2022-05-12 20:53:25 -0700

    main

--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ import React from "react";
 
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+root.render(<HelloMessage name="Taylor" />);
